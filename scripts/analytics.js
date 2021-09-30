185ececde946422a5ab58dae3d2e0cec410c3480
Matthew Jorgensen <matthew@jrgnsn.net>
2021-09-30 14:43:50 -0500

    Add outbound link tracking

--- scripts/analytics.js
+++ scripts/analytics.js
@@ -1,1 +1,1 @@
-<script async defer data-domain="tools.splat.soy" src="https://stats.jrgnsn.net/js/plausible.js"></script>
+<script async defer data-domain="tools.splat.soy" src="https://stats.jrgnsn.net/js/plausible.outbound-links.js"></script>
